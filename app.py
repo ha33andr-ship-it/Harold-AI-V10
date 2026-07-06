d4c2b7e86f26a13a08404744acc2e2a3e4968600
ha33andr-ship-it <ha33andr@gmail.com>
2026-07-06 11:21:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    return "Harold AI V10 Online!"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.json
+
+    print("Received Alert:")
+    print(data)
+
+    return jsonify({
+        "status": "success",
+        "message": "TradingView alert received",
+        "data": data
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
